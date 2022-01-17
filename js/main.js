d50973da1310754ec9635a214d07fe06790d1b10
Asan577 <asankanzada108@gmail.com>
2022-01-16 19:53:46 -0500

    first screen

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(function(){
 
-	$('.header-slider').slick({
-		dots: true,
-		prevArrow: '<button type="button" class="slick-prev">Previous</button>',
-		nextArrow: '<button type="button" class="slick-next">Next</button>',
+    $('.header-slider').slick({
+    dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
+        vertical: true
+    });
 
-	});
 
 
 
