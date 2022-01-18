d0177e683fa6343c8cff8d21b47efd961805a236
Asan577 <asankanzada108@gmail.com>
2022-01-17 22:18:23 -0500

    first screen

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ $(function(){
     dots: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
-        vertical: true
+        fade: true
     });
 
 
